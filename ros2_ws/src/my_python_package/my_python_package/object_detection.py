2ce78ea37ccee3ea0d35c09de87d55236dc13036
subash-taranga <subash-taranga@outlook.com>
2025-04-20 23:18:18 +0300

    yolo object detection untest

--- ros2_ws/src/my_python_package/my_python_package/object_detection.py
+++ ros2_ws/src/my_python_package/my_python_package/object_detection.py
@@ -4,35 +4,53 @@ from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 from std_msgs.msg import String
 from rclpy.qos import QoSProfile, ReliabilityPolicy
-
+import torch
+from pathlib import Path
+from sensor_msgs.msg import Image
 
 class ObjectDetection(Node):
 
     def __init__(self):
         super().__init__('object_detection')
-        topic = "/vel_cmd"
+        topic = "/camara_img"
         self.get_logger().info('object_detection is listening to Topic -> ' + topic)
         qos_profile = QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT)
-        self.sub = self.create_subscription(String, topic, self.chatter_callback, qos_profile)
+        self.sub = self.create_subscription(Image, topic, self.camara_frame_callback, qos_profile)
         
         # Create a publisher for the Twist message
         self.twist_pub = self.create_publisher(String, '/vel_cmd', 10)
 
         self.object_detection_string = String()
         
+        self.result = ""
+        
         # Create a timer to publish the Twist message
         timer_period = 1.0  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         
+        BASE = Path(__file__).parent.resolve()
+        WEIGHTS = BASE / "best.pt"
+        IMG     = BASE / "santa.JPG"  
 
-    def chatter_callback(self, msg: String):
-        self.get_logger().info(str(msg))
+        if not WEIGHTS.exists():
+            raise FileNotFoundError(f"Weights not found: {WEIGHTS}")
+        if not IMG.exists():
+            raise FileNotFoundError(f"Image not found:   {IMG}")
+
+        self.model = torch.hub.load(
+            "ultralytics/yolov5",   
+            "custom",               
+            path=str(WEIGHTS),      
+            force_reload=True       
+        )
+        
+
+    def camara_frame_callback(self, imageFrame: Image):
+        self.object_detection_string.data = self.model(str(imageFrame), augment=True)
 
     # demo program to move the steering
     def timer_callback(self):
-        self.object_detection_string.data = "test"
         self.twist_pub.publish(self.object_detection_string)
-        self.get_logger().info(f'Published...')
 
 def main(args=None):
     rclpy.init(args=args)
